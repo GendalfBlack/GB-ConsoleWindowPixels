5a9b3267b80764a2271678254db7f1c1a2e6564e
GendalfBlack <garmash_a@itstep.academy>
2020-12-01 09:34:17 +0200

    OOP refactoring
    Works for now for images with property width*3%4 == 0

--- WindowPixels/WindowPixels.cpp
+++ WindowPixels/WindowPixels.cpp
@@ -1,126 +1,105 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <string>
 #include <fstream>
 using namespace std;
 
-//path to the file
-const char* INPUT_FILENAME = "ban hammer.bmp";
-
-//simple header signature struct
+/// <summary>
+/// BMP fileinfo structure https://ru.wikipedia.org/wiki/BMP#/media/%D0%A4%D0%B0%D0%B9%D0%BB:BMPfileFormat.png
+/// </summary>
 struct BmpSignature
 {
     unsigned char data[2];
     BmpSignature() { data[0] = data[1] = 0; }
 };
-//simple header additional info struct
 struct BmpHeader
 {
     unsigned int fileSize;
     unsigned short reserved1;
     unsigned short reserved2;
     unsigned int dataOffset;
-    BmpHeader() : fileSize(0), reserved1(0), reserved2(0), dataOffset(0) { }
+    unsigned int headsize;
+    unsigned int width;
+    unsigned int height;
+    BmpHeader() : fileSize(0), reserved1(0), reserved2(0), dataOffset(0), width(0), height(0) {}
 };
-/// <summary>
-/// Function for filling structures of header info of .bmp file.
-/// </summary>
-/// <param name="fin">Address of the ifstream thread, who reads file.</param>
-/// <param name="sig">Address of the Signature of the file structure</param>
-/// <param name="header">Address of the Header of the file structure</param>
-void ReadHeader(ifstream& fin, BmpSignature& sig, BmpHeader& header)
-{
-    //if we in the file
-    if (!fin)
-        return;
-    //go to start of the bytes
-    fin.seekg(0, ios::beg);
-    //read and write to signature structure bytes
-    fin.read((char*)&sig, sizeof(sig));
-    //read and write to header structure bytes
-    fin.read((char*)&header, sizeof(header));
+
+class SpriteBMP {
+public: 
+    int width;
+    int height;
+    unsigned char*** raw_bytes;
+    BmpHeader hdr;
+    string name_of_file;
+
+    SpriteBMP(string name_of_file) : width(0), height(0) {
+        const char* INPUT_FILENAME = name_of_file.c_str();
+        this->name_of_file = name_of_file;
+        ifstream fin(INPUT_FILENAME, ios::binary);
+        if (fin.good())
+        {
+            BmpSignature sig;
+            if (!fin)
+                return;
+            fin.seekg(0, ios::beg);
+            fin.read((char*)&sig, sizeof(sig));
+            fin.read((char*)&hdr, sizeof(hdr));
+
+            width = hdr.width;
+            height = hdr.height;
+            raw_bytes = new unsigned char** [width];
+            for (int i = 0; i < width; i++)
+            {
+                raw_bytes[i] = new unsigned char* [height];
+                for (int j = 0; j < height; j++)
+                {
+                    raw_bytes[i][j] = new unsigned char[3];
+                    for (int k = 0; k < 3; k++)
+                    {
+                        raw_bytes[i][j][k] = ' ';
+                    }
+                }
+            }
+            fin.seekg(hdr.dataOffset, ios::beg);
+            unsigned char trash_can;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    fin >> this->raw_bytes[x][y][0];
+                    fin >> this->raw_bytes[x][y][1];
+                    fin >> this->raw_bytes[x][y][2];
+                }
+                if ((width * 3) % 4 != 0)
+                {
+                    for (int i = 0; i < (width * 3) % 4 + 1; i++)
+                    {
+                        fin >> trash_can;
+                    }
+                }
+            }
+        }
+        fin.close();
+    }
+};
+
+void DrawSpritePixels(SpriteBMP & sprite) {
+    HWND myconsole = GetConsoleWindow();
+    HDC mydc = GetDC(myconsole);
+    for (int i = 0; i < sprite.width; i++)
+    {
+        for (int j = 0; j < sprite.height; j++)
+        {
+            SetPixel(mydc, i, sprite.height - j, RGB(sprite.raw_bytes[i][j][2], sprite.raw_bytes[i][j][1], sprite.raw_bytes[i][j][0]));
+        }
+    }
 }
 
 int main()
 {
-    //get a console handle
-    HWND myconsole = GetConsoleWindow();
-    //get a handle to device context
-    HDC mydc = GetDC(myconsole);
-    //create a thread and send the path to the file, read binary
-    ifstream fin(INPUT_FILENAME, ios::binary);
-    //create signature structure
-    BmpSignature sig;
-    //create header structure
-    BmpHeader hdr;
-    //call structure filling function
-    ReadHeader(fin, sig, hdr);
-    //move to the end of header information
-    fin.seekg(hdr.dataOffset, 0);
-    //create 3d array for pixels
-    char *** pixels = new char**[100];
-    for (int i = 0; i < 100; i++)
-    {
-        pixels[i] = new char*[100];
-        for (int j = 0; j < 100; j++)
-        {
-            pixels[i][j] = new char[3];
-        }
-    }
-    //make every single pixel white
-    for (int i = 0; i < 100; i++)
-    {
-        for (int j = 0; j < 100; j++)
-        {
-            for (int k = 0; k < 3; k++)
-            {
-                pixels[i][j][k] = (char)255;
-            }
-        }
-    }
-
-    //create variables for the position of the current pixel 
-    int x = 0, y = 0;
-    //initialize bytes for each color of the pixels
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-    //go through file reading bytes
-    //cycle ends where the next byte is existing
-    while (fin >> r) {
-        //write to red value into array
-        pixels[x][y][0] = r; 
-        //move 1 byte ahead
-        fin >> g;
-        //write to green value into array
-        pixels[x][y][1] = g; 
-        fin >> b;
-        //write to blue value into array
-        pixels[x][y][2] = b; 
-        //move by 1 horizontally
-        x++;
-        //if we reach last pixel in a row
-        if (x > 99)
-        {
-            //reset position in the line
-            x = 0;
-            //move to the next line by 1 vertically
-            y++;
-        }
-    }
-    //go through 3d array of pixels
-    for (int i = 0; i < 100; i++)
-    {
-        for (int j = 0; j < 100; j++)
-        {
-            //create array of pixel info for readability of the next line
-            char * p = pixels[i][j];
-            //use SetPixel with:
-            //hendler for drawing
-            //cords inside picture x = i, y = 100 - j (y is upside down)
-            //RGB components from temporary array "p"
-            SetPixel(mydc, i, 100 - j, RGB((short)p[0], (short)p[1], (short)p[2]));
-        }
-    }
-    //move console output handle for sake of not removing our picture with end program info
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,10 });
+    SpriteBMP * s = new SpriteBMP("ban hammer4.bmp");
+    DrawSpritePixels(*s);
+    cin.get();
+    //SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,10 });
 }
